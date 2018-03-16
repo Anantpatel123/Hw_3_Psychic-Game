99a6231fde6106abd072acb02423c72aaa3db06b
Anant Patel <Anant1234@hotmail.com>
2018-03-15 20:47:40 -0400

    Final

---
+++ assets/javascript/game.js
@@ -0,0 +1,46 @@
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var possibleGuesses = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+  't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guessesSoFar = [];
+
+document.onkeyup = function (event) {
+
+  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+  if ((possibleGuesses.includes(userGuess))) {
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    guessesSoFar.push(userGuess);
+
+    if (userGuess === computerGuess) {
+      wins++ , guessesLeft = 9, guessesSoFar = [];
+      alert("You WON!!!!");
+    }
+    else if (guessesLeft === 0) {
+      losses++ , guessesLeft = 9, guessesSoFar = [];
+      alert("You Lost!!!")
+    }
+    else {
+      guessesLeft--;
+    }  
+
+
+    var html =
+      "<p>You chose: " + userGuess + "</p>" +
+      "<p>The computer chose: " + computerGuess + "</p>" +
+      "<p>wins: " + wins + "</p>" +
+      "<p>losses: " + losses + "</p>" +
+      "<p>Guess left: " + guessesLeft + "</p>" +
+      "<p>Your guesses so far: " + guessesSoFar + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+
+  }
+  
+}
